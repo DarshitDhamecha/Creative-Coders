444349248709c2224df26178e1a657815e51a8f7
DarshitDhamecha <darshit.dhamecha07@gmail.com>
2025-04-20 18:18:14 +0530

    add function for year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+
+    const currYear = new Date().getFullYear();
+
     return (
         <footer className='py-2 bg-[#2E5077]'>
-            <div className='main-section p-2 mx-auto flex items-center justify-between'>
-                <p className='tx-white text-lg font-bold'>© 2025 creativecodrs. All Rights Reserved</p>
+            <div className='main-section p-2 mx-auto md:flex-row flex-col gap-3 flex items-center justify-between'>
+                <p className='tx-white text-lg font-bold'>© {currYear} creativecodrs. All Rights Reserved</p>
                 <div className='flex tx-white gap-3.5 text-2xl'>
                     <i class="fa-brands fa-instagram"></i>
                     <i class="fa-brands fa-facebook"></i>
